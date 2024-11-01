e0ec380919de40c5472c06970245dce611039f34
dchansen06 <105574022+dchansen06@users.noreply.github.com>
2024-11-01 14:42:37 -0700

    Create scan200.sh

---
+++ scan200.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# setup
+rand=$(date +%s)
+printf "Content-type: text/html\n\n"
+
+# scan and turn into pdf
+scanimage --mode=Color --format=png --resolution=200 --device-name=airscan > /tmp/scan.png &&
+convert /tmp/scan.png /tmp/scan.pdf &&
+
+# make easy to edit name and make avaliable
+exiftool -Title= /tmp/scan.pdf &> /dev/null &&
+mv /tmp/scan.pdf /var/www/html/scanner/scan-$rand.pdf
+
+# clean up
+rm /tmp/scan.png
+rm /tmp/scan.pdf_original
+
+printf "scan-$rand.pdf"
